79d0cfd0f53c2bc34ac3eca376f4d05d5aad1fa6
Pichau <igorof06@outlook.com>
2024-10-24 19:51:12 -0300

    feat: starting project

---
+++ backendChallenge/src/test/java/com/backend_challenge/backendChallenge/BackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_challenge.backendChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
