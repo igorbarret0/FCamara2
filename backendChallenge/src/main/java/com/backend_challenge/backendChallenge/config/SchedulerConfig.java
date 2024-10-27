ffc3e3039da341a00e549225503df8c733328e61
Pichau <igorof06@outlook.com>
2024-10-27 17:58:58 -0300

    feat: enable spring scheduling

---
+++ backendChallenge/src/main/java/com/backend_challenge/backendChallenge/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.backend_challenge.backendChallenge.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
